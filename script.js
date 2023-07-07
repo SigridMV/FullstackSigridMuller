4a0f315aba4dd88331f5ea9e2c0c64b5a0d19b5f
Sigrid Muller <sigrid.muller.vidal@gmail.com>
2023-07-06 22:36:32 -0400

    Last review

--- script.js
+++ script.js
@@ -44,7 +44,11 @@ navToggleBtn.addEventListener("click", function () {
   elemToggleFunc(navbar);
   elemToggleFunc(document.body);
 
+
 });
+
+
+
 
 
 
@@ -105,3 +109,4 @@ if (localStorage.getItem("theme") === "light_theme") {
   document.body.classList.remove("light_theme");
   document.body.classList.add("dark_theme");
 }
+
